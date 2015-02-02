27f33ae6cbae9d65fad07c100e30d920cff7b95d
Breno Salgado <breno.salgado@avixy.com>
2015-02-02 09:42:05 -0200

    KeySetParam -> KeySetSlotParam

---
+++ src/main/java/com/avixy/qrtoken/negocio/servico/params/KeySetSlotParam.java
@@ -0,0 +1,11 @@
+package com.avixy.qrtoken.negocio.servico.params;
+
+/**
+ * Created by Ítalo on 1/15/2015.
+ */
+public class KeySetSlotParam extends ByteWrapperParam {
+
+    public KeySetSlotParam(byte keySet) {
+        super(keySet);
+    }
+}
