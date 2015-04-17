c5e408fa9dc490f6ef2f6a8aa9b884f12f30ab58
italo <italo@avixy.com>
2015-04-17 15:35:46 -0300

    Atualizacao no teste do servico de chave de sessao

--- src/test/java/com/avixy/qrtoken/negocio/servico/servicos/ebchat/EBChatShowSessionKeyServiceTest.java
+++ src/test/java/com/avixy/qrtoken/negocio/servico/servicos/ebchat/EBChatShowSessionKeyServiceTest.java
@@ -21,16 +21,14 @@ public class EBChatShowSessionKeyServiceTest {
     PasswordPolicy passwordPolicy = mock(PasswordPolicy.class);
     EBChatShowSessionKeyService service = new EBChatShowSessionKeyService(headerPolicy, timestampPolicy, messagePolicy, hmacKeyPolicy, passwordPolicy);
 
+    byte[] genericKey = new byte[32];
+
     @Before
     public void setUp() throws Exception {
-        byte[] genericKey = new byte[32];
 
         service.setHmacKey(genericKey);
         service.setAesKey(genericKey);
 
-        service.setChallenge("1234");
-        service.setSessionSecrecyKey(genericKey);
-        service.setSessionAuthKey(genericKey);
 
         when(messagePolicy.get(any())).thenReturn(new byte[0]);
         when(headerPolicy.getHeader(any())).thenReturn(new byte[0]);
@@ -41,6 +39,11 @@ public class EBChatShowSessionKeyServiceTest {
 
     @Test
     public void testMessage() throws Exception {
+        service.setChallenge("1234");
+        service.setSessionSecrecyKey(genericKey);
+        service.setSessionAuthKey(genericKey);
+
+
         String expectedMessage = "00000100" + //4 chars
                 "00110001001100100011001100110100" + // 1234
                 "00101" + //keylength 128 - 32bytes
@@ -52,6 +55,33 @@ public class EBChatShowSessionKeyServiceTest {
                 "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
                 "1111000101001100"//crc
                 ;
+
+        assertArrayEquals(BinaryMsg.get(expectedMessage), service.getMessage());
+    }
+
+    @Test
+    public void testMessage256() throws Exception {
+        byte[] key = new byte[32];
+        for (int i = 0; i < key.length; i++) {
+            key[i] = (byte)0x55;
+        }
+        service.setSessionAuthKey(key);
+        service.setSessionSecrecyKey(key);
+        service.setChallenge("1234");
+        String expectedMessage = "00000100" + "00110001" + "00110010" + "00110011" + "00110100" + // Length + Challenge "1234"
+        "00101" +		// KEY_LENGTH_256
+        "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101"  +// KEY 0X55 (16 TIMES)
+        "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" +
+        "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101"  +// KEY 0X55 (16 TIMES)
+        "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" +
+                "0111110100111111" +// CRC 0x7D3F
+
+        "00101" +		// KEY_LENGTH_256
+                "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101"  +// KEY 0X55 (16 TIMES)
+                "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" +
+                "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101"  +// KEY 0X55 (16 TIMES)
+                "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" + "01010101" +
+                "0111110100111111"; // CRC 0x7D3F
 
         assertArrayEquals(BinaryMsg.get(expectedMessage), service.getMessage());
     }
